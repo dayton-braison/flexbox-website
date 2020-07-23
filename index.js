f800402f11116fbbe67deb2b90fce76b53bfc1c8
Dayton Braison <daytonbraison@gmail.com>
2020-07-22 20:35:36 -0500

    JavaScript for website

---
+++ index.js
@@ -0,0 +1,10 @@
+const myButton = document.getElementById('my-button')
+myButton.style.height = '40px'
+myButton.style.width = '110px'
+myButton.style.background = '#7C77B9'
+myButton.style.color = 'white'
+myButton.style.fontWeight = '700'
+myButton.style.fontSize = '18px'
+myButton.addEventListener('click', function() {
+    alert('The journey is long, but stay motivated!')
+})
